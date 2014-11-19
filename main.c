1089cd5b5beb6dcc6c26e57a707b50a0bf0c5abd
Mike Mello <mellodogg101@gmail.com>
2014-11-18 23:01:48 -0800

    testing

--- main.c
+++ main.c
@@ -20,6 +20,7 @@ const int SCREEN_HEIGHT = 480;
 SDL_Window *window;
 SDL_Renderer *renderer;
 SDL_Surface *surface;
+SDL_Event event;
 
 // Boolean to test if the window is exited out
 bool quit = false;
@@ -37,6 +38,9 @@ void initSDL()
 							 );
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	surface = SDL_GetWindowSurface(window);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
 }
 
 void closeSDL()
@@ -48,9 +52,51 @@ void closeSDL()
 int main(int argc, char *argv[])
 {
 	initSDL();
+
 	
+	SDL_Rect player;
+	player.x = 10;
+	player.y = 10;
+	player.w = 30;
+	player.h = 30;
 	
-	
+	while(!quit)
+	{
+		if(SDL_PollEvent(&event))
+		{
+			switch(event.type)
+			{
+				case SDL_QUIT:
+					quit = true;
+					break;
+				case SDL_KEYDOWN:
+					switch(event.key.keysym.sym)
+					{
+						case SDLK_ESCAPE:
+							quit = true;
+							break;
+						case SDLK_LEFT:
+							player.x -= 10;
+							break;
+						case SDLK_RIGHT:
+							player.x += 10;
+							break;
+						case SDLK_UP:
+							player.y -= 10;
+							break;
+						case SDLK_DOWN:
+							player.y += 10;
+							break;
+					}
+					break;
+			}
+		}
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderFillRect(renderer, &player);
+		SDL_RenderPresent(renderer);
+	}
 	
 	closeSDL();
 	return 0;
